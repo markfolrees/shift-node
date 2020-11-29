375a52cba072d79e8548ae303a4a777f2ae89aef
Mark Rees <markrees@MacBook-Pro.local>
2020-11-29 11:34:17 +0000

    refactor handler

--- handler.js
+++ handler.js
@@ -1,3 +1,13 @@
+const processOrders = (body) => {
+  const allOrders = JSON.parse(body)
+  
+  return allOrders.ORDERS.filter(validOrder())
+}
+
+const validOrder = () => {
+  return o => o.O_ID === o.OMS_ORDER_ID
+}
+
 const handler = ({body}, context) => {
   const orders = processOrders(body)
   
@@ -5,9 +15,3 @@ const handler = ({body}, context) => {
 }
 
 module.exports = handler
-const processOrders = (body) => {
-  const allOrders = JSON.parse(body)
-  
-  return allOrders.ORDERS.filter(o => o.O_ID === o.OMS_ORDER_ID)
-}
-
